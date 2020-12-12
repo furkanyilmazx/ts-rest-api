918cbc3d9ae5a876e9df4cf9193bb0ca8c1e1753
Furkan YILMAZ <furkanyilmazx@gmail.com>
2020-12-12 13:12:26 +0300

    add dynamic port

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,6 @@ const a = 10;
 console.log(a);
 
 app.get('/health', (req, res) => res.send('up'));
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`⚡️[server]: Ssssddezrver is running at http://localhost:${PORT}`);
 });
